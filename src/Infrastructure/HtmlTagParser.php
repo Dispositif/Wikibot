1f737dbd58d5b355d8d098ce7bd3dc59c98ac7cb
Dispositif <dispositif@gmail.com>
2019-09-09 22:11:11 +0200

    HtmlTagParser

---
+++ src/Infrastructure/HtmlTagParser.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Infrastructure;
+
+use DOMDocument;
+use SimpleXMLElement;
+
+class HtmlTagParser
+{
+    /*
+     * @var SimpleXMLElement
+     */
+    protected $xml;
+
+    public function __construct() { }
+
+    /**
+     * @param string $data
+     *
+     * @return $this
+     */
+    public function importHtml(string $data)
+    {
+        libxml_use_internal_errors(true);
+        $doc = new DOMDocument();
+        $doc->strictErrorChecking = false;
+        $doc->loadHTML(
+            mb_convert_encoding($data, 'HTML-ENTITIES', 'UTF-8')
+        );
+        $this->xml = simplexml_import_dom($doc);
+
+        return $this;
+    }
+
+    /**
+     * @param string $path
+     *
+     * @return array
+     * @throws \Exception
+     */
+    public function xpath(string $path): array
+    {
+        if (!$this->xml instanceof SimpleXMLElement) {
+            throw new \Exception('XML non défini');
+        }
+        $nodes = $this->xml->xpath($path); // SimpleXMLElement[]
+
+        $refs = [];
+
+        // todo? extract in function xml2String() ?
+        foreach ($nodes as $key => $node) {
+            $raw = trim((string)$node);
+            // filter the empty <ref name="bla" />
+            if (strlen($raw) > 1) {
+                $refs[] = $raw;
+            }
+        }
+
+        return $refs;
+    }
+
+}
