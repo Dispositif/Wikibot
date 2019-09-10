3d07e5a230157775837de0179b182044eb539c8f
Dispositif <dispositif@gmail.com>
2019-09-10 17:47:33 +0200

    boostrap reserved ?

--- src/Application/myBootstrap.php
+++ src/Application/myBootstrap.php
@@ -7,7 +7,7 @@ $bootstrap = true;
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 
-require __DIR__.'/../../vendor/autoload.php';
+include __DIR__.'/../../vendor/autoload.php';
 
 $dotEnv = new Dotenv();
 $dotEnv->load(__DIR__.'/../../.env');
