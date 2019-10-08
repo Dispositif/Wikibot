c2ae4874c1c5f161109d36fb5de41005763ed0d0
Phil M <dispositif@gmail.com>
2019-10-08 21:02:23 +0200

    SubjectTrait

---
+++ src/Infrastructure/SubjectTrait.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Infrastructure;
+
+use SplObserver;
+use SplSubject;
+
+/**
+ * TODO: not used
+ * Observer pattern. For class which implements SplSubject.
+ * Trait SubjectTrait
+ *
+ * @package App\Infrastructure
+ */
+trait SubjectTrait
+{
+    protected $observers = [];
+
+    public function attach(SplObserver $observer)
+    {
+        $this->observers[] = $observer;
+
+        return $this;
+    }
+
+    public function detach(SplObserver $observer)
+    {
+        if (is_int($key = array_search($observer, $this->observers, true))) {
+            unset($this->observers[$key]);
+        }
+
+        return $this;
+    }
+
+    public function notify()
+    {
+        foreach ($this->observers as $observer) {
+            /**
+             * @var $observer SplObserver
+             */
+            $observer->update($this);
+        }
+    }
+
+}
