698bafe30deb4f1a45d0588658eae6bf21064117
Phil M <dispositif@gmail.com>
2019-10-08 21:44:37 +0200

    style

--- src/Application/myBootstrap.php
+++ src/Application/myBootstrap.php
@@ -15,11 +15,6 @@ include __DIR__.'/../../vendor/autoload.php';
 
 $dotEnv = new Dotenv();
 $dotEnv->load(__DIR__.'/../../.env');
-
 ini_set("user_agent", getenv('USER_AGENT'));
-
-//ini_set('xdebug.var_display_max_depth', '10');
-//ini_set('xdebug.var_display_max_children', '256');
-//ini_set('xdebug.var_display_max_data', '1024');
 
 
