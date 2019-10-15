c211ee6cdccfc31a3efceb3bd67244cf41005f62
Phil M <dispositif@gmail.com>
2019-10-15 15:40:52 +0200

    Add queue

--- src/Application/WstatWorker.php
+++ src/Application/WstatWorker.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace App\Application;
 
+use App\Domain\IsbnFacade;
 use App\Domain\Utils\TemplateParser;
 use App\Infrastructure\CorpusAdapter;
+use App\Infrastructure\MessageAdapter;
 use App\Infrastructure\WstatImport;
 use GuzzleHttp\Client;
 use Symfony\Component\Console\Style\SymfonyStyle;
@@ -19,13 +21,13 @@ class WstatWorker extends FileWorker
         echo "To exit press CTRL+C\n";
 
         $wstat = new WstatImport(
-            new Client(['timeout' => 2]), [
+            new Client(['timeout' => 10]), [
             'title' => 'Ouvrage',
             'query' => 'inclusions',
             'param' => 'isbn',
             'start' => 50000,
-            'limit' => 50,
-        ], 100
+            'limit' => 500,
+        ], 1000
         );
         $datas = $wstat->getData();
 
@@ -44,9 +46,8 @@ class WstatWorker extends FileWorker
             if (!empty($tpData['isbn2'])) {
                 $this->saveIsbn($data['title'], $tpData['isbn2']);
             }
-        }
 
-        // TODO : title/isbn -> sql
+        }
 
         // TODO data title/template -> SQL
         // TODO entity+doctrine ?
@@ -65,7 +66,7 @@ class WstatWorker extends FileWorker
                 $e->getLine(),
                 $string
             );
-            // TODO log
+            // TODO log?
             $tpData = [];
         }
 
@@ -74,8 +75,19 @@ class WstatWorker extends FileWorker
 
     public function saveISBN(string $page, string $isbn): void
     {
-        // todo ISBN validator
-        echo "SAVE : $page, $isbn \n";
+        $isbnMachine = new IsbnFacade($isbn);
+        // validate ISBN
+        if(!$isbnMachine->isValid($isbn)) {
+            echo sprintf("Notice : ISBN %s not valid \n", $isbn);
+            return;
+        }
+
+        $data = ['page' => $page, 'isbn' => $isbn];
+        $msg = json_encode($data);
+
+        (new MessageAdapter())->amqpMsg('isbnRequest', $msg);
+        echo "Queue isbnRequest: $page, $isbn \n";
+        sleep(1);
     }
 
     public function process($io)
