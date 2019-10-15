fe3f59417daf91211a5601141a0576a02138e5dd
Phil M <dispositif@gmail.com>
2019-10-15 20:49:38 +0200

    FileWorker

---
+++ src/Application/FileWorker.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application;
+
+use App\Infrastructure\CorpusAdapter;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+class FileWorker
+{
+    /**
+     * @param SymfonyStyle|null $io
+     */
+    public function run(?SymfonyStyle $io = null)
+    {
+        $corp = new CorpusAdapter();
+
+        echo "WORKER ".__CLASS__."\n";
+        echo "To exit press CTRL+C\n";
+
+
+        while (true) {
+            $this->process($io);
+            sleep(5);
+        }
+    }
+
+    public function process($io)
+    {
+        $name = $io->ask('Continue ?');
+        echo time()."\n";
+    }
+}
+//
+//$console = new Application('worker', '0.1');
+//$console->addCommand(new WorkerCommand());
+//$worker = new FileWorker();
+//$worker->run();
