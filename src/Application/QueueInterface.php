a49fe2ce40100c6af43b57ddd02e17e3d7df3504
Phil M <dispositif@gmail.com>
2019-10-18 00:27:51 +0200

    Add QueueInterface

---
+++ src/Application/QueueInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Application;
+
+interface QueueInterface
+{
+    public function getNewRaw(): ?string;
+
+    public function sendCompletedData(array $finalData):bool;
+
+}
