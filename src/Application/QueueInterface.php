022111d49cf96417912d35fae2c11c3a1dfaa1f7
Phil M <dispositif@gmail.com>
2019-10-18 12:18:11 +0200

    Style copyright

--- src/Application/QueueInterface.php
+++ src/Application/QueueInterface.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * This file is part of dispositif/wikibot application
+ * 2019 (c) Philippe M. <dispositif@gmail.com>
+ * For the full copyright and license information, please view the LICENSE file
+ */
 
 declare(strict_types=1);
 
