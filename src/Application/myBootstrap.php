3d4ba7ecac631e1d8f3cd339a556837788d8e68f
Phil <dispositif@gmail.com>
2019-11-22 17:56:30 +0100

    Fix bootstrap
    Travis build failed

--- src/Application/myBootstrap.php
+++ src/Application/myBootstrap.php
@@ -9,8 +9,6 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
-$bootstrap = true;
-
 error_reporting(E_ALL);
 ini_set('display_errors', 'on');
 //set_time_limit(60);
@@ -19,6 +17,12 @@ date_default_timezone_set('Europe/Paris');
 
 include __DIR__.'/../../vendor/autoload.php';
 
-$dotEnv = new Dotenv();
-$dotEnv->load(__DIR__.'/../../.env');
-ini_set('user_agent', getenv('USER_AGENT'));
+$dotFilename = __DIR__.'/../../.env';
+if (file_exists($dotFilename)) {
+    $dotEnv = new Dotenv();
+    $dotEnv->load($dotFilename);
+}
+if (getenv('USER_AGENT')) {
+    ini_set('user_agent', getenv('USER_AGENT'));
+}
+
