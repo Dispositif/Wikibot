51b1a2ea872c51ff9c0137e6a318d917af2e3e0a
Phil <dispositif@gmail.com>
2019-11-23 16:35:59 +0100

    Migration db

--- src/Infrastructure/resources/database_schema.sql
+++ src/Infrastructure/resources/database_schema.sql
@@ -1,24 +1,27 @@
 CREATE TABLE `TempRawOpti`
 (
     `id`            int(11) unsigned NOT NULL AUTO_INCREMENT,
-    `page`          varchar(150)          DEFAULT NULL,
+    `page`          varchar(150)     NOT NULL DEFAULT '',
     `raw`           text,
     `opti`          text,
     `opticorrected` text,
-    `optidate`      timestamp        NULL DEFAULT NULL,
-    `modifs`        varchar(150)          DEFAULT NULL,
-    `version`       varchar(10)           DEFAULT NULL,
-    `notcosmetic`   int(11)               DEFAULT NULL,
-    `major`         int(11)               DEFAULT NULL,
-    `isbn`          varchar(20)           DEFAULT NULL,
-    `edited`        timestamp        NULL DEFAULT NULL,
-    `priority`      int(11)               DEFAULT '0',
-    `tocorrect`     tinyint(4)            DEFAULT '0',
-    `corrected`     timestamp        NULL DEFAULT NULL,
-    `torevert`      tinyint(4)            DEFAULT '0',
-    `reverted`      timestamp        NULL DEFAULT NULL,
-    `row`           timestamp        NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `optidate`      timestamp        NULL     DEFAULT NULL,
+    `skip`          tinyint(1)                DEFAULT '0',
+    `modifs`        varchar(150)              DEFAULT NULL,
+    `version`       varchar(10)               DEFAULT NULL,
+    `notcosmetic`   int(11)                   DEFAULT NULL,
+    `major`         int(11)                   DEFAULT NULL,
+    `isbn`          varchar(20)               DEFAULT NULL,
+    `edited`        timestamp        NULL     DEFAULT NULL,
+    `priority`      int(11)                   DEFAULT '0',
+    `tocorrect`     tinyint(4)                DEFAULT '0',
+    `corrected`     timestamp        NULL     DEFAULT NULL,
+    `torevert`      tinyint(4)                DEFAULT '0',
+    `reverted`      timestamp        NULL     DEFAULT NULL,
+    `row`           timestamp        NULL     DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+    `verify`        timestamp        NULL     DEFAULT NULL,
+    `altered`       int(11)                   DEFAULT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
-  AUTO_INCREMENT = 16038
+  AUTO_INCREMENT = 16037
   DEFAULT CHARSET = utf8;
