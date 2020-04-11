cac941bc9b207471cfb84046e08b8c45a2df4eb9
Dispositif <dispositif@gmail.com>
2020-04-11 07:32:15 +0200

    WIP WebMapperWorker

--- src/Application/Examples/WebMapperWorker.php
+++ src/Application/Examples/WebMapperWorker.php
@@ -15,7 +15,7 @@ use App\Application\WikiBotConfig;
 use App\Infrastructure\CirrusSearch;
 use App\Infrastructure\ServiceFactory;
 
-include __DIR__.'/../myBootstrap.php';
+include __DIR__.'/../ZiziBot_Bootstrap.php';
 
 /**
  * TODO move PROCESS FROM BOTTOM
@@ -23,9 +23,9 @@ include __DIR__.'/../myBootstrap.php';
  *
  * @package App\Application\Examples
  */
-class Ref2ArticleWorker extends AbstractBotTaskWorker
+class WebMapperWorker extends AbstractBotTaskWorker
 {
-    const TASK_NAME           = "Amélioration bibliographique : URL ⇒ {Article}"; // 😎
+    const TASK_NAME           = "Amélioration bibliographique : URL ⇒ modèle"; // 😎
     const SLEEP_AFTER_EDITION = 120;
     protected $botFlag = false;
     protected $modeAuto = true;
@@ -50,8 +50,9 @@ class Ref2ArticleWorker extends AbstractBotTaskWorker
 
 // &srqiprofile=popular_inclinks_pv&srsort=last_edit_desc
 $cirrusURL
-    = 'https://fr.wikipedia.org/w/api.php?action=query&list=search&srsearch=%22https%3A%2F%2Fwww.lemonde.fr%22+insource%3A%2F%5C%3Cref%5C%3Ehttps%5C%3A%5C%2F%5C%2Fwww%5C.lemonde%5C.fr%5B%5E%5C%3E%5D%2B%5C%3C%5C%2Fref%3E%2F&formatversion=2&format=json&srnamespace=0&srlimit=100&srqiprofile=popular_inclinks_pv&srsort=random';
+    = 'https://fr.wikipedia.org/w/api.php?action=query&list=search&srsearch=%22https%3A%2F%2Fwww.lemonde.fr%22+insource%3A%2F%5C%3Cref%5C%3Ehttps%5C%3A%5C%2F%5C%2Fwww%5C.lemonde%5C.fr%5B%5E%5C%3E%5D%2B%5C%3C%5C%2Fref%3E%2F&formatversion=2&format=json&srnamespace=0&srlimit=1000&srqiprofile=popular_inclinks_pv&srsort=random';
 $wiki = ServiceFactory::wikiApi();
 $botConfig = new WikiBotConfig();
 $cirrusList = new CirrusSearch($cirrusURL);
-new Ref2ArticleWorker($botConfig, $wiki, $cirrusList);
+new OldRef2ArticleWorker($botConfig, $wiki, $cirrusList);
+
