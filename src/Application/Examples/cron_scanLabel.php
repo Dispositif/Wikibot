74364db98d4e357eb0e0a7e527f4fd63fc6cca9a
Dispositif <dispositif@gmail.com>
2020-09-02 18:21:31 +0200

    Add cron_scanLabel process : Propositions BA/AdQ

---
+++ src/Application/Examples/cron_scanLabel.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * This file is part of dispositif/wikibot application
+ * 2019 : Philippe M. <dispositif@gmail.com>
+ * For the full copyright and MIT license information, please view the LICENSE file.
+ */
+
+declare(strict_types=1);
+
+namespace App\Application\Examples;
+
+use App\Application\ScanWiki2DB;
+use App\Application\WikiBotConfig;
+use App\Infrastructure\DbAdapter;
+use App\Infrastructure\PageList;
+use App\Infrastructure\ServiceFactory;
+
+
+include __DIR__.'/../myBootstrap.php';
+
+/**
+ * From a titles list, scan the wiki and add the {ouvrage} citations into the DB
+ */
+
+$wiki = ServiceFactory::wikiApi();
+
+// Catégories : Article potentiellement bon, Article potentiellement de qualité
+echo "Catégories : Article potentiellement bon, Article potentiellement de qualité\n";
+$ba = PageList::FromWikiCategory('Article potentiellement de qualité');
+$adq = PageList::FromWikiCategory('Article potentiellement bon');
+$list = new PageList(array_merge($ba->getPageTitles(), $adq->getPageTitles()));
+
+new ScanWiki2DB($wiki, new DbAdapter(), new WikiBotConfig(), $list, 20);
