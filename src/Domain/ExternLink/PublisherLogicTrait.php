e121f187adc1ae7edc6b118eb58c04233021961d
Dispositif <dispositif@gmail.com>
2023-04-18 13:34:57 +0200

    Refac and fix PublisherLogicTrait

--- src/Domain/ExternLink/PublisherLogicTrait.php
+++ src/Domain/ExternLink/PublisherLogicTrait.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 
 namespace App\Domain\ExternLink;
 
+use App\Domain\Models\Wiki\AbstractWikiTemplate;
 use App\Domain\Models\Wiki\ArticleTemplate;
 use App\Domain\Models\Wiki\LienWebTemplate;
 use App\Domain\Models\Wiki\OuvrageTemplate;
@@ -22,7 +23,7 @@ trait PublisherLogicTrait
      */
     protected function importConfigAndData(): void
     {
-        // todo REFAC DataObject[]
+        // todo REFAC DataObject[] (WTF?)
         $this->config = Yaml::parseFile(self::CONFIG_PRESSE);
         $skipFromFile = file(
             self::SKIP_DOMAIN_FILENAME,
@@ -47,16 +48,23 @@ trait PublisherLogicTrait
 
     /**
      * TODO rename/move Postprocess data site/périodique
-     * Logique : remplacement titre périodique ou nom du site
-     *
-     * @param array $mapData
-     * @param       $template
-     *
-     * @return array
+     * Replace 'site' and 'périodique' with wikidata URL of newspapers and scientific journals or config_press (YAML).
      */
-    protected function replaceSitenameByConfig(array $mapData, $template): array
+    protected function replaceSitenameByConfig(array $mapData, AbstractWikiTemplate $template): array
     {
-        // from wikidata URL of newspapers
+        // Wikidata replacements
+        $mapData = $this->replaceWithWDataNewspaper($mapData, $template);
+        $mapData = $this->replaceWithWDataSciences($mapData);
+
+        // config_press YAML replacements
+        return $this->replaceWithHumanConfig($template, $mapData);
+    }
+
+    /**
+     * Replace 'site' with wikidata URL of newspapers
+     */
+    protected function replaceWithWDataNewspaper(array $mapData, AbstractWikiTemplate $template): array
+    {
         if (!empty($this->publisherData['newspaper'][$this->registrableDomain])) {
             $frwiki = $this->publisherData['newspaper'][$this->registrableDomain]['frwiki'];
             $label = $this->publisherData['newspaper'][$this->registrableDomain]['fr'];
@@ -67,26 +75,104 @@ trait PublisherLogicTrait
                 $mapData['périodique'] = WikiTextUtil::wikilink($label, $frwiki);
             }
         }
+        return $mapData;
+    }
 
-        // from wikidata of scientific journals
+    /**
+     * Replace 'périodique' with wikidata URL of scientific journals
+     */
+    protected function replaceWithWDataSciences(array $mapData): array
+    {
         if (isset($mapData['périodique']) && isset($this->publisherData['scientific wiki'][$mapData['périodique']])) {
             $mapData['périodique'] = WikiTextUtil::wikilink(
                 $mapData['périodique'],
                 $this->publisherData['scientific wiki'][$mapData['périodique']]
             );
         }
+        return $mapData;
+    }
 
-        // from YAML config
-        if (!empty($this->config[$this->registrableDomain]['site']) && $template instanceof LienWebTemplate) {
+    /**
+     * Replace 'site' and 'périodique' with config_press (YAML)
+     */
+    protected function replaceWithHumanConfig(AbstractWikiTemplate $template, array $mapData): array
+    {
+        $mapData = $this->replaceSiteForLienWeb($template, $mapData);
+        $mapData = $this->replacePeriodique($mapData, $template);
+
+        $mapData = $this->replaceEditor($mapData);
+
+        $mapData = $this->stripParamValue('stripfromauthor', $mapData, 'auteur1');
+
+        return $this->stripParamValue('stripfromtitle', $mapData, 'titre');
+    }
+
+    /**
+     * @param AbstractWikiTemplate $template
+     * @param array $mapData
+     *
+     * @return array
+     */
+    protected function replaceSiteForLienWeb(AbstractWikiTemplate $template, array $mapData): array
+    {
+        if (isset($this->config[$this->registrableDomain]['site']) && $template instanceof LienWebTemplate) {
             $mapData['site'] = $this->config[$this->registrableDomain]['site'];
+            if (empty($mapData['site'])) {
+                unset($mapData['site']);
+            }
         }
-        if (!empty($this->config[$this->registrableDomain]['périodique'])
+        return $mapData;
+    }
+
+    /**
+     * @param array $mapData
+     * @param AbstractWikiTemplate $template
+     *
+     * @return array
+     */
+    protected function replacePeriodique(array $mapData, AbstractWikiTemplate $template): array
+    {
+        if (isset($this->config[$this->registrableDomain]['périodique'])
             && (!empty($mapData['périodique'])
                 || $template instanceof OuvrageTemplate)
         ) {
             $mapData['périodique'] = $this->config[$this->registrableDomain]['périodique'];
+            if (empty($mapData['périodique'])) {
+                unset($mapData['périodique']);
+            }
         }
+        return $mapData;
+    }
 
+    /**
+     * Replace 'éditeur' on {article} todo move
+     */
+    protected function replaceEditor(array $mapData): array
+    {
+        if (isset($this->config[$this->registrableDomain]['éditeur'])
+            && !empty($mapData['éditeur'])) {
+            $mapData['éditeur'] = $this->config[$this->registrableDomain]['éditeur'];
+            if (empty($mapData['éditeur'])) {
+                unset($mapData['éditeur']);
+            }
+        }
+        return $mapData;
+    }
+
+    /**
+     * Use config_press stripFromAuthor (string array) and stripFromTitle, to remove text from author and title.
+     */
+    protected function stripParamValue(string $configParam, array $mapData, string $templateParam): array
+    {
+        if (!empty($this->config[$this->registrableDomain][$configParam])
+            && isset($this->config[$this->registrableDomain][$configParam])
+            && !empty($mapData[$templateParam])) {
+            $stripText = $this->config[$this->registrableDomain][$configParam]; // string|array
+            $mapData[$templateParam] = trim(str_ireplace($stripText, '', $mapData[$templateParam]));
+            if (empty($mapData[$templateParam])) {
+                unset($mapData[$templateParam]);
+            }
+        }
         return $mapData;
     }
 
