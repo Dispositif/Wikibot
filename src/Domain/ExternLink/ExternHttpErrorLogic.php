f2b4f162687d82b9a8f95a0a363d6ba130eae447
Dispositif <dispositif@gmail.com>
2023-10-10 01:13:50 +0200

    ExternHttpErrorLogic : add HTTP 500 and DNS error as dead link

--- src/Domain/ExternLink/ExternHttpErrorLogic.php
+++ src/Domain/ExternLink/ExternHttpErrorLogic.php
@@ -15,6 +15,7 @@ use Psr\Log\NullLogger;
 class ExternHttpErrorLogic
 {
     public const LOG_REQUEST_ERROR = __DIR__ . '/../../Application/resources/external_request_error.log';
+    protected const LOOSE = true;
 
     public function __construct(
         protected DeadLinkTransformer $deadLinkTransformer,
@@ -34,29 +35,49 @@ class ExternHttpErrorLogic
             }
             return $url;
         } // 403
-        elseif (preg_match('#403 Forbidden#i', $errorMessage)) {
+        if (preg_match('#403 Forbidden#i', $errorMessage)) {
             $this->log403($url);
 
             return $url;
-        } elseif (preg_match('#404 Not Found#i', $errorMessage)) {
+        }
+        if (preg_match('#404 Not Found#i', $errorMessage)) {
             $this->log->notice('404 Not Found');
 
             if (ExternRefTransformer::REPLACE_404) {
                 return $this->deadLinkTransformer->formatFromUrl($url);
             }
             return $url;
-        } elseif (preg_match('#401 Unauthorized#i', $errorMessage)) {
+        }
+        if (preg_match('#401 Unauthorized#i', $errorMessage)) {
             $this->log->notice('401 Unauthorized : skip ' . $url);
 
             return $url;
-        } else {
+        }
+
+
+        if (self::LOOSE && preg_match('#500 Internal Server Error#i', $errorMessage)) {
+            $this->log->notice('500 Internal Server Error');
+
+            return $this->deadLinkTransformer->formatFromUrl($url);
+        }
+        if (self::LOOSE && preg_match("#cURL error 97: Can't complete SOCKS5 connection#i", $errorMessage)) {
+            $this->log->notice('error 97: Can\'t complete SOCKS5');
+
+            return $this->deadLinkTransformer->formatFromUrl($url);
+        }
+
+        // DEFAULT (not filtered)
             //  autre : ne pas générer de {lien brisé}, car peut-être 404 temporaire
-            $this->log->warning('erreur sur extractWebData ' . $errorMessage);
+            // "URL rejected: No host part in the URL (see https://curl.haxx.se/libcurl/c/libcurl-errors.html)
+            // "cURL error 28: Connection timed out after 20005 milliseconds (see https://curl.haxx.se/libcurl/c/libcurl-errors.html)
+            //"cURL error 28: Connection timed out after 20005 milliseconds (see https://curl.haxx.se/libcurl/c/libcurl-errors.html)
+            //"cURL error 97: Can't complete SOCKS5 connection to www.ewppp.org. (4) (see https://curl.haxx.se/libcurl/c/libcurl-errors.html)
+            // "cURL error 97: Can't complete SOCKS5 connection to www.dzhand.net
+            $this->log->notice('erreur non gérée sur extractWebData: "' . $errorMessage . "\" URL: ".$url);
 
             //file_put_contents(self::LOG_REQUEST_ERROR, $this->domain."\n", FILE_APPEND);
 
-            return $url;
-        }
+        return $url;
     }
 
     protected function log403(string $url): void
